575dc0a012a5973a4560e84511e95c05270e6038
Taotoyota67 <tawtoyota67@gmail.com>
2023-10-20 13:56:29 +0700

    Test3

--- concept demo/good_boy.py
+++ concept demo/good_boy.py
@@ -1,12 +1,12 @@
-class Good_Boy() :
-    def __init__(self, hp = 30, hp2 = 100, hit = 5, hit2 = 10) :
+class Good_Boy():
+    def __init__(self, hp=30, hp2=100, hit=5, hit2=10):
         self.hp = hp
         self.hp2 = hp2
         self.hit = hit
         self.hit2 = hit2
 
-    def heal(self) :
+    def heal(self):
         self.hp += 10
 
-    def charge_at(self) :
+    def charge_at(self):
         self.hit2 = 20
