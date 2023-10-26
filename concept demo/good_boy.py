4126924fed00111e32d658b3327dd425a3d58871
Taotoyota67 <tawtoyota67@gmail.com>
2023-10-27 03:11:03 +0700

    making class more useful

--- concept demo/good_boy.py
+++ concept demo/good_boy.py
@@ -1,12 +1,36 @@
 class Good_Boy():
-    def __init__(self, hp=30, hp2=100, hit=5, hit2=10):
-        self.hp = hp
-        self.hp2 = hp2
-        self.hit = hit
-        self.hit2 = hit2
+	def __init__(self,
+			hp=100,
+			max_hp=100,
+			hit=10,
+			got_hit=["Tsk...", "So annoying",
+				"Is that all you got", "AH..."],
+			do_hit=["W E A K",
+				"let me show you.", "I'm not holding back."],
+			do_heal=["Did you think it will end that easily.", "Not yet",
+				"Come Onnnn"],
+			skill=["heal", "charge"],
+			do_charge=["You will regret this.", "PEASANT!!!", "Rot in hell"]):
+		self.hp = hp
+		self.max_hp = max_hp
+		self.hit = hit
+		self.got_hit = got_hit
+		self.do_hit = do_hit
+		self.do_heal = do_heal
+		self.skill = skill
+		self.do_charge = do_charge
 
-    def heal(self):
-        self.hp += 10
+	def heal(self) :
+		if self.hp + 15 <= 100 :
+			self.hp += 15
+		else :
+			self.hp = 100
+			
+	def charge(self) :
+		self.hit += 10
 
-    def charge_at(self):
-        self.hit2 = 20
+	def lose_hp(self, player_damage) :
+		if self.hp - player_damage >= 0 :
+			self.hp -= player_damage
+		else :
+			self.hp = 0
---
+++ concept demo/player.py
@@ -0,0 +1,24 @@
+class Player():
+	def __init__(self,hp=100, hit=10, mana=100):
+		self.hp = hp
+		self.hit = hit
+		self.mana = mana
+
+	def lose_hp(self, monster_damage) :
+		if self.hp - monster_damage >= 0 :
+			self.hp -= monster_damage
+		else :
+			self.hp = 0
+
+
+	def lose_mana(self) :
+		if self.mana - 10 >= 0 :
+			self.mana -= 10
+		else :
+			self.mana = 0
+
+	def heal(self):
+		if self.hp + 10 <= 100 :
+			self.hp += 10
+		else :
+			self.hp = 100
