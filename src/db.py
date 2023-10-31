8133db60dc76b59953c45db4820c4c3ea6ec6feb
Tpmonkey <nuttee.2548@hotmail.com>
2023-10-31 20:06:06 +0700

    db supports 'in' and 'del'

--- src/db.py
+++ src/db.py
@@ -13,8 +13,14 @@ class Database:
             json.dump(self._data, f, indent=4)
 
     def __getitem__(self, __name: str) -> Any:
-        return self._data.get(__name)
+        return self._data[__name]
 
     def __setitem__(self, __name: str, __value: Any) -> None:
         self._data[__name] = __value
         self.save()
+
+    def __contains__(self, item: str) -> bool:
+        return item in self._data
+
+    def __delitem__(self, key: str) -> None:
+        del self._data[key]
