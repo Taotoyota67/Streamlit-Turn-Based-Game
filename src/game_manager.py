d26ffae3db9803d91df8e93c6185dba770d5568a
Tpmonkey <nuttee.2548@hotmail.com>
2023-10-31 20:05:23 +0700

    Add game menu screen

--- src/game_manager.py
+++ src/game_manager.py
@@ -11,19 +11,23 @@ class GameManager:
         self.st = st
         self.db = db
         self.acc = acc
-        self.cols = st.columns([1, 2])
+        self.col1, self.col2 = None, None
         self.page = st.empty()
         self.playerdata = None
         self.main_page()
+        # self.stats_page()
 
-    def clear_page(self) -> None:
+    def clear_page(self, clear: bool = True) -> None:
         self.page.empty()
-        self.page = self.st.empty()
+        if clear:
+            self.page = self.st.empty()
 
     def main_page(self) -> None:
         self.clear_page()
         self.page.title("Welcome to THE GAME")
         self.st.write("Is this your first time?")
+
+        # Buttons
         self.st.button("Yes", on_click=self.create_account_page)
         self.st.button("No", on_click=self.login_page)
 
@@ -33,9 +37,13 @@ class GameManager:
         self.st.write("What is your username?")
         self.st.write(
             "*The Confirm button will be disable if your username sucks.*")
+
+        # Text input
         text = self.st.text_input(
             "Username", max_chars=16, on_change=self.create_account_page,
             placeholder="Enter a username", label_visibility="hidden")
+
+        # Buttons
         self.st.button("Confirm", on_click=self.create_password_page,
                        disabled=self.acc.check_username(text),
                        args=(text,))
@@ -45,10 +53,14 @@ class GameManager:
         self.clear_page()
         self.page.title("Good name!")
         self.st.write(f"Now **{username}**, What is your password?")
+
+        # Text input
         password = self.st.text_input("password", placeholder="Enter a password",
                                       on_change=self.create_password_page,
                                       label_visibility="hidden",
                                       type="password", args=(username,))
+
+        # Buttons
         self.st.button("Confirm", on_click=self.create_account,
                        disabled=len(password) < 3, args=(username, password))
         self.st.button("Back", on_click=self.create_account_page)
@@ -59,22 +71,27 @@ class GameManager:
         try:
             self.acc.add(username, password)
         except AccountAlreadyExists:
-            return self.create_account_page()
+            self.create_account_page()
+            return
         sleep(2)
         self.login(username, password)
 
-    def login_page(self, success: bool=True):
+    def login_page(self, success: bool = True):
         self.clear_page()
         self.page.title(
             "Welcome back!" if success else "Incorrect username/password :("
         )
         self.st.write("I promise I won't leak it.")
+
+        # Text inputs
         username = self.st.text_input(
             "Username", max_chars=16, on_change=self.login_page,
             placeholder="Enter a username", label_visibility="hidden")
         password = self.st.text_input("password", placeholder="Enter a password",
                                       on_change=self.login_page, label_visibility="hidden",
                                       type="password")
+
+        # Buttons
         self.st.button("Login", on_click=self.login, args=(username, password))
         self.st.button("Back", on_click=self.main_page)
 
@@ -83,14 +100,24 @@ class GameManager:
         self.page.title("Logging in...")
 
         success = self.acc.login(username, password)
-        sleep(2)
+        # sleep(2)
         if not success:
             self.login_page(success)
             return
 
         self.playerdata = PlayerData(username, self.db)
-        self.game_page()
+        self.stats_page()
 
-    def game_page(self):
-        self.clear_page()
-        self.st.write("Yes")
+    def stats_page(self):
+        self.page.empty()
+
+        # Work around of an error 'setIn' cannot be called on an ElementNode
+        # Which happen when I make columns and empty object at the same time.
+        self.col1, self.col2 = self.st.columns([1, 3])
+
+        self.col1.title("Menu")
+        self.col1.button("Stats", on_click=self.stats_page)
+        self.col1.button("Upgrade", on_click=self.stats_page)
+        self.col1.button("Test", on_click=self.stats_page)
+
+        self.col2.title("Monster and Menu details goes here.")
