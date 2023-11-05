0ff72d88924c39296c4d89a6ed45c454e6753ef1
Tpmonkey <nuttee.2548@hotmail.com>
2023-11-05 12:43:54 +0700

    Add docstring

--- src/game.py
+++ src/game.py
@@ -3,6 +3,9 @@ from classes.player import Player
 
 
 class Game:
+    """Game state per player.
+    """
+
     def __init__(self, username: str) -> None:
         self.username = username.lower()
         self.player = Player(self.username)
