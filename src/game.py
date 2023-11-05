9fcb441ebc6fe0f34d26b6ad896d3d70728c1753
Tpmonkey <nuttee.2548@hotmail.com>
2023-11-05 12:23:23 +0700

    Remove unused db import

--- src/game.py
+++ src/game.py
@@ -1,8 +1,8 @@
-from db import db
+# from db import db # Not used, yet
 from classes.player import Player
 
 
 class Game:
     def __init__(self, username: str) -> None:
         self.username = username.lower()
-        self.player = Player(username)
+        self.player = Player(self.username)
